4f30519d4e0b237c2a4d500c49c929238b9a3264
anjalivs990 <anjalivs990@gmail.com>
2025-12-11 17:29:14 +0530

    added resend mail service

--- ECommerce/src/main/java/com/Anjali/ECommerce/Service/EmailService.java
+++ ECommerce/src/main/java/com/Anjali/ECommerce/Service/EmailService.java
@@ -1,15 +1,14 @@
 package com.Anjali.ECommerce.Service;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -17,30 +16,19 @@ public class EmailService {
 
     private final JavaMailSender javaMailSender;
 
-    @Value("${spring.mail.username:#{null}}")
-    private String springMailUsername;
-
-    @Value("${spring.mail.password:#{null}}")
-    private String springMailPassword;
-
     public void sendVerificationOtpEmail(String userEmail, String otp, String subject, String text) throws MessagingException {
         try {
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
 
-            helper.setSubject(subject);
-            helper.setText(text);
-            helper.setTo(userEmail);
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "UTF-8");
+
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(text);
+            mimeMessageHelper.setTo(userEmail);
 
             javaMailSender.send(mimeMessage);
         } catch (MailException e) {
-            throw new MailSendException("Failed to send Email", e);
+            throw new MailSendException("Failed to send Email");
         }
-    }
-
-    @PostConstruct
-    public void debugMailConfig() {
-        System.out.println("🌸 MAIL USER FROM SPRING = " + springMailUsername);
-        System.out.println("🌸 MAIL PASS FROM SPRING = " + springMailPassword);
     }
 }
