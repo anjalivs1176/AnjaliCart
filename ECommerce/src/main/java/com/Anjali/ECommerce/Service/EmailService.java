2dbd76371cd3468ab634b3835a67637c9c5be9c9
anjalivs990 <anjalivs990@gmail.com>
2025-12-11 17:10:40 +0530

    fixed emailservice

--- ECommerce/src/main/java/com/Anjali/ECommerce/Service/EmailService.java
+++ ECommerce/src/main/java/com/Anjali/ECommerce/Service/EmailService.java
@@ -1,8 +1,10 @@
 package com.Anjali.ECommerce.Service;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -12,22 +14,33 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class EmailService {
+
     private final JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username:#{null}}")
+    private String springMailUsername;
+
+    @Value("${spring.mail.password:#{null}}")
+    private String springMailPassword;
 
     public void sendVerificationOtpEmail(String userEmail, String otp, String subject, String text) throws MessagingException {
         try {
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
 
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "UTF-8");
-
-            mimeMessageHelper.setSubject(subject);
-            mimeMessageHelper.setText(text);
-            mimeMessageHelper.setTo(userEmail);
+            helper.setSubject(subject);
+            helper.setText(text);
+            helper.setTo(userEmail);
 
             javaMailSender.send(mimeMessage);
+        } catch (MailException e) {
+            throw new MailSendException("Failed to send Email", e);
         }
-        catch (MailException e) {
-            throw new MailSendException("Failed to send Email");
-        }
+    }
+
+    @PostConstruct
+    public void debugMailConfig() {
+        System.out.println("🌸 MAIL USER FROM SPRING = " + springMailUsername);
+        System.out.println("🌸 MAIL PASS FROM SPRING = " + springMailPassword);
     }
 }
