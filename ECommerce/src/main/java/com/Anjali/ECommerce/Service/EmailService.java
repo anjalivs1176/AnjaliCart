cb4ec7ef9a5c386571546f82bb3fa8a41f47f847
anjalivs990 <anjalivs990@gmail.com>
2025-12-12 10:19:00 +0530

    updated application-prod properties

--- ECommerce/src/main/java/com/Anjali/ECommerce/Service/EmailService.java
+++ ECommerce/src/main/java/com/Anjali/ECommerce/Service/EmailService.java
@@ -1,41 +1,6 @@
 package com.Anjali.ECommerce.Service;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
+public interface EmailService {
 
-import jakarta.mail.internet.MimeMessage;
-
-@Service
-public class EmailService {
-
-    private final JavaMailSender javaMailSender;
-
-    @Value("${mail.from}")
-    private String from;
-
-    public EmailService(JavaMailSender javaMailSender) {
-        this.javaMailSender = javaMailSender;
-    }
-
-    public void sendVerificationOtpEmail(String userEmail, String otp, String subject, String text) {
-        try {
-            MimeMessage message = javaMailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
-
-            helper.setFrom(from);
-            helper.setTo(userEmail);
-            helper.setSubject(subject);
-            helper.setText(text + "\n\nYour OTP: " + otp, false);
-
-            javaMailSender.send(message);
-
-            System.out.println("Email sent to " + userEmail);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Failed to send OTP email", e);
-        }
-    }
+    void sendVerificationOtpEmail(String userEmail, String otp, String subject, String text);
 }
